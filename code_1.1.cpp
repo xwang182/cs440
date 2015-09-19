175ed2632630f9ee02292c3afda388a0aa289225
Xuan Wang <xwang182@illinois.edu>
2015-09-19 17:06:06 -0500

    A* get

--- code_1.1.cpp
+++ code_1.1.cpp
@@ -20,10 +20,19 @@ class node {
 	    int visited; //1 is visited, 0 is not
 	    int inlist;
 	    int dist;
+	    int cost;
 
+	//for Greedy compare
+	// bool operator<(const node& right) const {
+ //    	return (this->dist > right.dist);
+	// }
+
+
+	//for A* compare
 	bool operator<(const node& right) const {
-    	return (this->dist > right.dist);
+    	return (this->dist+this->cost > right.dist + right.cost);
 	}
+
 
 };
 
@@ -65,6 +74,7 @@ int main () {
 			map[i][k].prevY=0;
 			map[i][k].visited=0;
 			map[i][k].inlist=0;
+			map[i][k].cost=0;
 		}
 	}
 	int lineNum=0;
@@ -275,6 +285,93 @@ int main () {
 
 
 
+
+    //Greedy
+ //    int height=alllines;
+ //    int width=eachlines;
+	// priority_queue<node> greedy;
+	// map[startX][startY].prevX=startX;
+	// map[startX][startY].prevY=startY;
+	// greedy.push(map[startX][startY]);
+	// map[startX][startY].inlist=1;
+	// node smallest;
+	// for(;;){
+	// 	smallest=greedy.top();
+	// 	currentX=smallest.selfX;
+	// 	currentY=smallest.selfY;
+
+	// 	greedy.pop();
+	// 	if(smallest.selfX==endX  && smallest.selfY==endY ){
+	// 		break;
+	// 	}
+		
+
+ //    if(currentX+1<height-1){
+	//      if( map[currentX+1][currentY].state!='%'   && map[currentX+1][currentY].inlist==0  ){
+	//      	map[currentX+1][currentY].inlist=1;
+	//      	map[currentX+1][currentY].prevX=currentX;
+	//      	map[currentX+1][currentY].prevY=currentY;
+	// 		greedy.push(map[currentX+1][currentY]);
+	// 		}		
+	// 	}
+
+	//  if(currentY+1<width-1){
+	//      if( map[currentX][currentY+1].state!='%'   && map[currentX][currentY+1].inlist==0  ){
+	//      	map[currentX][currentY+1].inlist=1;
+	//      	map[currentX][currentY+1].prevX=currentX;
+	//      	map[currentX][currentY+1].prevY=currentY;
+	// 		greedy.push(map[currentX][currentY+1]);
+	// 		}		
+	// 	}	
+
+	// 	 if(currentX-1>0){
+	//      if( map[currentX-1][currentY].state!='%'   && map[currentX-1][currentY].inlist==0  ){
+	//      	map[currentX-1][currentY].inlist=1;
+	//      	map[currentX-1][currentY].prevX=currentX;
+	//      	map[currentX-1][currentY].prevY=currentY;
+	// 		greedy.push(map[currentX-1][currentY]);
+	// 		}		
+	// 	}	
+	
+	// 	 if(currentY-1>0){
+	//      if( map[currentX][currentY-1].state!='%'   && map[currentX][currentY-1].inlist==0  ){
+	//      	map[currentX][currentY-1].inlist=1;
+	//      	map[currentX][currentY-1].prevX=currentX;
+	//      	map[currentX][currentY-1].prevY=currentY;
+	// 		greedy.push(map[currentX][currentY-1]);
+	// 		}		
+	// 	}	
+	
+		
+
+
+	// }
+
+
+
+	// node temp=map[endX][endY];
+	// for(;;){
+	// 	if(temp.selfX==startX && temp.selfY==startY){
+	// 		break;
+	// 	}
+	// 	temp.state='.';
+	// 	map[temp.selfX][temp.selfY]=temp;
+
+	// 	temp=map[temp.prevX][temp.prevY];
+	// }
+
+	// for(int i=0;i<alllines;i++){
+	// 	for(int k=0;k<eachlines;k++){
+	// 		cout << map[i][k].state;
+	// 	}
+ //    	cout<<endl;
+	// }
+
+
+
+
+
+    // A*
     int height=alllines;
     int width=eachlines;
 	priority_queue<node> greedy;
@@ -299,6 +396,7 @@ int main () {
 	     	map[currentX+1][currentY].inlist=1;
 	     	map[currentX+1][currentY].prevX=currentX;
 	     	map[currentX+1][currentY].prevY=currentY;
+	     	map[currentX+1][currentY].cost=map[currentX][currentY].cost+1;
 			greedy.push(map[currentX+1][currentY]);
 			}		
 		}
@@ -308,6 +406,7 @@ int main () {
 	     	map[currentX][currentY+1].inlist=1;
 	     	map[currentX][currentY+1].prevX=currentX;
 	     	map[currentX][currentY+1].prevY=currentY;
+	     	map[currentX][currentY+1].cost=map[currentX][currentY].cost+1;
 			greedy.push(map[currentX][currentY+1]);
 			}		
 		}	
@@ -317,6 +416,7 @@ int main () {
 	     	map[currentX-1][currentY].inlist=1;
 	     	map[currentX-1][currentY].prevX=currentX;
 	     	map[currentX-1][currentY].prevY=currentY;
+	     	map[currentX-1][currentY].cost=map[currentX][currentY].cost+1;
 			greedy.push(map[currentX-1][currentY]);
 			}		
 		}	
@@ -326,6 +426,7 @@ int main () {
 	     	map[currentX][currentY-1].inlist=1;
 	     	map[currentX][currentY-1].prevX=currentX;
 	     	map[currentX][currentY-1].prevY=currentY;
+	     	map[currentX][currentY-1].cost=map[currentX][currentY].cost+1;
 			greedy.push(map[currentX][currentY-1]);
 			}		
 		}	
